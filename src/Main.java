de3c413669ed7f91cbf806820b27d3eca9a5ec63
Alessandro <alex97av@gmail.com>
2023-07-15 22:19:06 +0200

    aggiunta variabili booleani(a.b.c.d.f.t)

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main (String[] args){
+       boolean a= 2 <= 2 && !true;
+       boolean b = !false && 3 > 2;
+       boolean t;
+       boolean f;
+       boolean c= (t=false)(f=true): !(!t || f);
+       boolean d= 6 > 6 ^ !(true && true)
+
+    }
+}
