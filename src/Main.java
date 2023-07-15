12d64b77883d402f59a21966ee8d023ab50f0a33
Alessandro <alex97av@gmail.com>
2023-07-15 23:40:22 +0200

    aggiunta variabili, aggiunta commenti, aggiunta print che verifica i dati, esercizio completato

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main (String[] args){
-       boolean a= 2 <= 2 && !true;
-       boolean b = !false && 3 > 2;
-       boolean t;
-       boolean f;
-       boolean c= (t=false)(f=true): !(!t || f);
-       boolean d= 6 > 6 ^ !(true && true)
+       boolean a= 2 <= 2  && !true; //nella prima parte il 2 <= 2 è vero, l'operatore Not(!) trasforma !true in false  quindi l'operatore AND(&&) confronta vero&&false risultato finale FALSE
+       boolean b = !false && 3 > 2; //l'operatore Not(!) trasforma !false in true, 3>2=true,l'operatore AND(&&) true&&true risultato finale TRUE
+       boolean t=false;
+       boolean f=true;
+       boolean c= !(!t || f); //l'operatore Not(!) trasforma !t in true,f=true,l'operatore OR || true||true=true ma l'operatore Not(!) trasforma il valore finale in FALSE
+       boolean d= 6 > 6 ^ !(true && true); //6>6 = False,l'operatore Not(!) trasforma !(true) in false, l'operatore Logical Exclusive OR (^) trovando valori uguali restituise un FALSE
+       System.out.println("Exercise A="+a+"\nExercise B="+b+"\nExercise C="+c+"\nExercise D="+d);
 
     }
 }
+
